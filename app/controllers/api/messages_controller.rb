06b8cf595ea92ed43811d9f0d62fa6319bb9364a
GikaiKamito <gikaikamito@gikaikamitonoMacBook-Air.local>
2019-05-10 16:16:21 +0900

    修正

--- app/controllers/api/messages_controller.rb
+++ app/controllers/api/messages_controller.rb
@@ -2,7 +2,7 @@ class Api::MessagesController < ApplicationController
   def index
     @messages = Message.all
     respond_to do |format| 
-    format.json { @new_message = @messages.where( 'id > ?', params[:id]) }
+      format.json { @new_message = @messages.where( 'id > ?', params[:id]) }
     end
   end
 end
